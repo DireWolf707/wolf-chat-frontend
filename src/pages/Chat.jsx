2607c27e3b9029c65a37e167386cd3ce81b1c10d
DireWolf707 <rahulsood707@gmail.com>
2023-05-30 12:40:17 +0530

    added breakpoints

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,11 +1,22 @@
-import { Stack } from "@mui/material"
+import { useMediaQuery, Stack } from "@mui/material"
 import Sidebar from "../components/chat/Sidebar"
 import ChatSection from "../components/chat/ChatSection"
-import {} from "../store"
+import { useParams } from "react-router-dom"
 
 const Chat = () => {
+  const { chatRoomId } = useParams()
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"))
+
+  if (isSmall)
+    return (
+      <Stack flexGrow={1} flexDirection="row" gap={2} overflow="auto" p="16px">
+        {!chatRoomId && <Sidebar />}
+        {chatRoomId && <ChatSection />}
+      </Stack>
+    )
+
   return (
-    <Stack flexGrow={1} flexDirection="row" overflow="auto">
+    <Stack flexGrow={1} flexDirection="row" gap={2} overflow="auto" p="16px">
       <Sidebar />
       <ChatSection />
     </Stack>
